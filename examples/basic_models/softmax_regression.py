25e2878a666750226f1a0630af00a554c2f60f36
rusty1s <matthias.fey@tu-dortmund.de>
2016-11-05 11:01:40 +0100

    completed softmax example

--- examples/basic_models/softmax_regression.py
+++ examples/basic_models/softmax_regression.py
@@ -6,16 +6,12 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('/tmp/mnist_data/', one_hot=True)
 
-train_images, train_labels = mnist.train.next_batch(5000)
-test_images, test_labels = mnist.test.next_batch(200)
-
 img_size = 28
 img_size_flat = img_size * img_size
 
-train_x = tf.placeholder(tf.float32, [None, img_size_flat])
-test_x = tf.placeholder(tf.float32, [img_size_flat])
+x = tf.placeholder(tf.float32, [None, img_size_flat])
 
-# Softmax Regressions:
+# Softmax:
 #
 # We want to be able to look at an image and give the probabilities for it 
 # being each digit, e.g. our model might look at a picture of a nine and be 80% 
@@ -23,19 +19,38 @@ test_x = tf.placeholder(tf.float32, [img_size_flat])
 # probability to all the others because it isn't 100% sure.
 # This is a classic case where a softmax regression is the thing to do, because 
 # softmax gives us a list of values between 0 and 1 that add up to 1.
-#
-# A softmax regression has two steps:
-# 1. We add up the evidence of our input being in certain classes. To tally up 
-#    the evidence that a given image is in a particular class, we do a weighted 
-#    sum of the pixel intensities over all training images over all classes.  
-#    The weight is negative if that pixel having is negative if that pixel 
-#    having a high intensity is evidence against the image being in that class, 
-#    and positive if it is evidence in favor.
-#    We also add some extra evidence called a bias, to be able to say that some 
-#    things are more likely independent of the input.
-# 2. We convert that evidence into probabilities.
-
 weight = tf.Variable(tf.zeros([img_size_flat, 10]))
 bias = tf.Variable(tf.zeros([10]))
 
-softmax = tf.nn.softmax(tf.matmul(train_x, weight) + bias)
+evidence = tf.matmul(x, weight) + bias
+
+# Training:
+#
+# The network is trained by minimizing a cost functin called cross-entropy.
+y = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(evidence, y))
+
+# We minimize the cross-entropy by gradient descent with a learning rate of 0.5
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+# Testing:
+#
+# We evaluate the network by comparing the real label with the computed highest 
+# label and computing the accuracy over all input images x.
+correct_prediction = tf.equal(tf.argmax(evidence, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# initialize the graph
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+# train the network 1000 times
+for _ in range(1000):
+    batch_images, batch_labels = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={ x: batch_images, y: batch_labels })
+
+# run the test
+print('Train accuracy:', sess.run(accuracy, feed_dict= { x: mnist.train.images, y: mnist.train.labels }))
+
+print('Test accuracy:', sess.run(accuracy, feed_dict= { x: mnist.test.images, y: mnist.test.labels }))
